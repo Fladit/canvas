b7f1c2ce1ced534f7c52045f59e0f9fa4a690750
Fladit <zatsarin77@gmail.com>
2020-11-28 14:22:15 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/app.scss'
+import Canvas from "./components/Canvas";
+import SettingBar from "./components/SettingBar";
+import Toolbar from "./components/Toolbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"app"}>
+          <Toolbar/>
+          <SettingBar/>
+          <Canvas/>
+      </div>
   );
-}
+};
 
 export default App;
