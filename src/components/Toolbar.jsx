a685800de7670608a630331fd19152199f9eb5d3
Fladit <zatsarin77@gmail.com>
2020-11-29 12:01:52 +0300

    add main tools in toolbar

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -4,7 +4,15 @@ import "../styles/toolbar.scss"
 const Toolbar = () => {
     return (
         <div className={"toolbar"}>
-
+            <button className={"btn brush"}/>
+            <button className={"btn rect"}/>
+            <button className={"btn circle"}/>
+            <button className={"btn eraser"}/>
+            <button className={"btn line"}/>
+            <input type={"color"} className={"inputColor"}/>
+            <button className={"btn undo"}/>
+            <button className={"btn redo"}/>
+            <button className={"btn save"}/>
         </div>
     );
 };
