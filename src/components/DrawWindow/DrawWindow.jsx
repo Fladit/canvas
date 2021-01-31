16cea9d487fb2671ee0e56dc0c79e3fb8690d9f9
Fladit <zatsarin77@gmail.com>
2021-01-31 13:29:34 +0300

    add redirect for wrong url in draw window withoud back-end handling

--- src/components/DrawWindow/DrawWindow.jsx
+++ src/components/DrawWindow/DrawWindow.jsx
@@ -2,23 +2,36 @@ import React, {useEffect} from 'react';
 import Canvas from "./Canvas/Canvas";
 import Toolbar from "./Toolbar/Toolbar";
 import SettingBar from "./SettingBar/SettingBar";
-import {useParams} from "react-router";
+import {useHistory, useParams} from "react-router";
 import {observer} from "mobx-react-lite";
 import WebSocketHandler from "../../utils/WebSocketHandler";
 import SocketStore from "../../store/SocketStore";
 
+const uidLength = {
+    UUID_V4: 36, // 9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d
+    UID: 16, // eug1612088335172
+}
+
 const DrawWindow = observer(() => {
     const params = useParams()
+    const history = useHistory()
     useEffect(() => {
         const sessionID = params.uid
         console.log(sessionID)
-        const socket = new WebSocket(`ws://localhost:5000/draw/${sessionID}?token=${localStorage.getItem("token")}`)
-        //SocketStore.setSocket(socket, sessionID)
-        WebSocketHandler.setSocket(socket, sessionID)
-        return () => {
-            socket.close()
-            SocketStore.setSocket(null, null)
-            console.log("socket закрыт")
+        if (sessionID.length === uidLength.UID || sessionID.length === uidLength.UUID_V4) {
+            const socket = new WebSocket(`ws://localhost:5000/draw/${sessionID}?token=${localStorage.getItem("token")}`)
+            //SocketStore.setSocket(socket, sessionID)
+            WebSocketHandler.setSocket(socket, sessionID)
+            return () => {
+                socket.close()
+                SocketStore.setSocket(null, null)
+                console.log("socket закрыт")
+            }
+        }
+        else {
+            alert("Введён неверный UID сессии!")
+            // Заменить, когда будет готова страница с ошибкой при вводе неправильного url
+            history.push("/")
         }
     }, [])
     return (
