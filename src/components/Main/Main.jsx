319b60b537dc99b9254d5c61ef4a49d53e1cb346
Fladit <zatsarin77@gmail.com>
2021-02-11 21:53:14 +0300

    add Main, Navbar and Menu component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Menu from "./Menu/Menu";
 import "../../styles/main.css"
+import Navbar from "./Navbar";
 
 const Main = () => {
     return (
         <div className={"main"}>
-            <Menu/>
+            <Navbar/>
+            <div className={"content"}>
+            </div>
         </div>
     )
 }
