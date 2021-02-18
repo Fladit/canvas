3db12348871a507e8257f8c1f22a213131bae38f
Fladit <zatsarin77@gmail.com>
2021-02-18 21:07:34 +0300

    add button enter key handling

--- src/utils/Button/Button.jsx
+++ src/utils/Button/Button.jsx
@@ -4,9 +4,15 @@ import "../../styles/button.css"
 const Button = ({isDisabled, clickFunction, buttonLabel}) => {
     return (
         <button className={"button-auth"} disabled={isDisabled}
-                    onClick={clickFunction}>{buttonLabel}
+                onClick={clickFunction}
+                onKeyPress={(e) => {handleKeyDown(e, clickFunction)}}>{buttonLabel}
         </button>
     );
 };
+
+function handleKeyDown(e, func) {
+    if (e.key === "Enter")
+        func()
+}
 
 export default Button;
